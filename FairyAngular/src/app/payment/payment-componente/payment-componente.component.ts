e400d29324f0c9cb9c7bd1920bd44ec5bfcfb261
Jessica Rodríguez <jdprincesita@gmail.com>
2024-03-04 18:36:47 -0300

    se agrega la seccion de pagos

---
+++ FairyAngular/src/app/payment/payment-componente/payment-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-componente',
+  templateUrl: './payment-componente.component.html',
+  styleUrl: './payment-componente.component.css'
+})
+export class PaymentComponenteComponent {
+
+}
---
+++ FairyAngular/src/app/payment/payment.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaymentComponenteComponent } from './payment-componente/payment-componente.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaymentComponenteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaymentModule { }
