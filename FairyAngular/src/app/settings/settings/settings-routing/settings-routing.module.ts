8b770ae655d4701a53b0c7843aa0ebfed5577314
Jessica Rodríguez <jdprincesita@gmail.com>
2024-03-05 14:52:53 -0300

    se logra enrutar a settings

--- FairyAngular/src/app/settings/settings/settings-routing/settings-routing.module.ts
+++ FairyAngular/src/app/settings/settings/settings-routing/settings-routing.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+// Importar los componentes que se van a leer
+import { SettingsComponenteComponent } from '../settings-componente/settings-componente.component';
+import {BasicInfoComponenteComponent} from "../../basic-info/basic-info-componente/basic-info-componente.component";
+import { AuthListComponent } from '../../../auth/auth-list/auth-list.component';
+import { AuthPermissionsComponent } from '../../../auth/auth-permissions/auth-permissions.component';
+/*
+import {AuthInsertComponent} from "../auth-insert/auth-insert.component";
+import {AuthListComponent} from "../auth-list/auth-list.component";
+import {AuthUpdateComponent} from "../auth-update/auth-update.component";
+import {AuthPermissionsComponent} from "../auth-permissions/auth-permissions.component"; 
+*/
 
 
+const routes: Routes = [{
+  path:'',
+  component: SettingsComponenteComponent,
+  children: [
+      {path: 'basic-info', component: BasicInfoComponenteComponent},
+      //{path: 'add-rol', component: AuthInsertComponent},
+      {path: 'list-rol', component: AuthListComponent},
+      {path: 'list-group-perission', component: AuthPermissionsComponent},
+  ],
+},];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule,FormsModule,RouterModule.forChild(routes)],
+  exports:[RouterModule]
 })
 export class SettingsRoutingModule { }
+
+
+
+//
--- FairyAngular/src/app/settings/settings/settings.module.ts
+++ FairyAngular/src/app/settings/settings/settings.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SettingsComponenteComponent } from './settings-componente/settings-componente.component';
 import {SettingsService} from './settings.service';
+import { SettingsRoutingModule } from './settings-routing/settings-routing.module';
 
 @NgModule({
   declarations: [SettingsComponenteComponent],//colocar todos los componentes entre comas
-  imports: [CommonModule,FormsModule,RouterModule],//colocar los modulos entre comas
+  imports: [CommonModule,SettingsRoutingModule,FormsModule,RouterModule],//colocar los modulos entre comas
   providers:[SettingsService]
 })
 export class SettingsModule { }
