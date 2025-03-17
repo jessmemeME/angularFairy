83d5211a76c617ee0e6c43b7238f48016496cf37
alvarortizd <36858097+alvarortizd@users.noreply.github.com>
2025-03-16 23:12:39 -0300

    agregar disenho a servicios

--- FairyAngular/src/app/services/components/servicios/servicios.component.ts
+++ FairyAngular/src/app/services/components/servicios/servicios.component.ts
@@ -6,19 +6,52 @@ import { ServiciosService } from '../../servicios.service';
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.scss']
 })
-export class ServiciosComponent implements OnInit {
-  servicios: any[] = [];
-  displayedColumns: string[] = ['codigo', 'nombre', 'categoria', 'subcategoria', 'estado', 'acciones'];
+export class ServiciosComponent   {
+ // Esquema de colores para los gráficos
+ colorScheme = {
+  domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+};
 
-  constructor(private serviciosService: ServiciosService) {}
-
-  ngOnInit(): void {
-    this.obtenerServicios();
+// Datos para gráficos de barras
+barChartData = [
+  {
+    "name": "Enero",
+    "value": 5000
+  },
+  {
+    "name": "Febrero",
+    "value": 7200
+  },
+  {
+    "name": "Marzo",
+    "value": 6100
+  },
+  {
+    "name": "Abril",
+    "value": 7800
   }
-
-  obtenerServicios(): void {
-    this.serviciosService.getServicios().subscribe(data => {
-      this.servicios = data;
-    });
+];
+// Datos para gráfico de torta (pie chart)
+pieChartData = [
+  {
+    "name": "Catering",
+    "value": 35
+  },
+  {
+    "name": "Música",
+    "value": 25
+  },
+  {
+    "name": "Decoración",
+    "value": 20
+  },
+  {
+    "name": "Fotografía",
+    "value": 20
   }
+];
+
+  constructor( ) {}
+
+   
 }
