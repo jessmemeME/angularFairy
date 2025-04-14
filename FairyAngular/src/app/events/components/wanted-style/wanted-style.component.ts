f869d48eccc7af70745b1cdcc1664dd5bbaf8090
alvarortizd <36858097+alvarortizd@users.noreply.github.com>
2025-04-13 22:44:11 -0300

    add new component to handle the ideas selection

--- FairyAngular/src/app/events/components/wanted-style/wanted-style.component.ts
+++ FairyAngular/src/app/events/components/wanted-style/wanted-style.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil, tap, finalize } from 'rxjs/operators';
+import { BriefRepository } from '../../repositories/brief.repository';
+import { Category, Segment, Style } from '../../models/category.interface';
 @Component({
   selector: 'app-wanted-style',
   templateUrl: './wanted-style.component.html',
   styleUrl: './wanted-style.component.css'
 })
-export class WantedStyleComponent {
+export class WantedStyleComponent implements OnInit, OnDestroy{
+  categories: Category[] = [];
+  isLoading = false;
+  private destroy$ = new Subject<void>();
 
+  constructor(private categoryRepo: BriefRepository) {}
+
+  ngOnInit(): void {
+    this.categoryRepo.getAll().pipe(
+      tap(() => this.isLoading = true),
+      finalize(() => this.isLoading = false),
+      takeUntil(this.destroy$)
+    ).subscribe({
+      next: (data) => this.categories = data,
+      error: (err) => console.error('🧨 Error desde componente:', err.message)
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
